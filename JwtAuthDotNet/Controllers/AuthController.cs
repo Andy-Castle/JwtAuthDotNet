168292e83ef03def1f642efb5e0e899233bf6204
Andy Castle <masterchief_and@live.com>
2025-02-10 11:38:07 -0500

    El JWT ya se crea, cuando el usuario se registrar y luego hace login este recibe su token

--- JwtAuthDotNet/Controllers/AuthController.cs
+++ JwtAuthDotNet/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
@@ -54,10 +55,19 @@ namespace JwtAuthDotNet.Controllers
             };
 
             var key = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings: Token")!));
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
 
-            var creds = 
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
 
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings: Issuer"),
+                audience: configuration.GetValue<string>("AppSettings: Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
         }
     }
 }
