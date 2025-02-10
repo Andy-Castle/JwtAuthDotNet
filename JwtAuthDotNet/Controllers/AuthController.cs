8331fa10b4f63b4dd918b287b787efdfcbe4aea1
Andy Castle <masterchief_and@live.com>
2025-02-10 11:06:05 -0500

    Add project files.

---
+++ JwtAuthDotNet/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using JwtAuthDotNet.Entities;
+using JwtAuthDotNet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthDotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IConfiguration configuration) : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                  .HashPassword(user, request.Password);
+
+            user.UserName = request.UserName;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.UserName != request.UserName)
+            {
+                return BadRequest("User nit found");
+            }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings: Token")!));
+
+            var creds = 
+
+        }
+    }
+}
